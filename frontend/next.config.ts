85446dbb6e7f61ca23e98cbc85dcbff6b51191b5
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2025-10-08 16:41:09 +0545

    wip: added external domain for next image tag

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,28 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '5000',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'sriyog-app-dashboard.onrender.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
